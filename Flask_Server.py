321de4317387ce4b2c6bb407c6645e01953b6b36
NULLt1 <m.otec@gmx.de>
2020-01-05 15:19:37 +0100

    added power route

--- Flask_Server.py
+++ Flask_Server.py
@@ -8,5 +8,11 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/power/')
+def power():
+    print("power")
+    return render_template('home.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
