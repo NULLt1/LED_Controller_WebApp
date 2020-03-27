1044fdec32041f591fc757f235e8455deb8a51ce
NULLt1 <motec@in-technology.eu>
2020-03-27 13:40:37 +0100

    added slider

--- static/js/main.js
+++ static/js/main.js
@@ -6,5 +6,18 @@ function power() {
 }
 
 
+var RGBChange = function() {
+	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+};
+
+var r = $('#R').slider()
+		.on('slide', RGBChange)
+		.data('slider');
+var g = $('#G').slider()
+		.on('slide', RGBChange)
+		.data('slider');
+var b = $('#B').slider()
+		.on('slide', RGBChange)
+		.data('slider');
 
 });
