54d8f75429f08539f2bdca90aad42e5efeaefb1d
NULLt1 <m.otec@gmx.de>
2020-06-16 13:13:12 +0200

    inital rgb color

--- static/js/main.js
+++ static/js/main.js
@@ -5,19 +5,25 @@ function power() {
     location.href = '/power/'
 }
 
+$("button[name ='color-button']").each(function( index ) {
+  let red = $(this).attr('red');
+  let green = $(this).attr('green');
+  let blue = $(this).attr('blue');
+  $(this).css("background-color", "rgb("+red+","+green+","+blue+")");
+  $(this).click(function() {
+    changeColorRequest(red,green,blue);
+  });
+});
 
-var RGBChange = function() {
-	$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-};
 
-var r = $('#R').slider()
-		.on('slide', RGBChange)
-		.data('slider');
-var g = $('#G').slider()
-		.on('slide', RGBChange)
-		.data('slider');
-var b = $('#B').slider()
-		.on('slide', RGBChange)
-		.data('slider');
+
+
+
+
+
+function changeColorRequest(red,green,blue) {
+    console.log(red+green+blue);
+    $.post( "/color/", { red: red, green: green, blue: blue } );
+}
 
 });
